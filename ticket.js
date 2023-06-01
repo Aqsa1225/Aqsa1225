f26c99d4007deb18c668beeff3e66f85cdbfdb7d
AQSA RIAZ <aqsabathspa.ae@gmail.com>
2023-06-01 12:26:00 +0400

    done 7

--- ticket.js
+++ ticket.js
@@ -1,5 +1,10 @@
 document.getElementById("ticket-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("message").style.display = "block";
-  });
+  // Add event listener to the form with ID "ticket-form" and listen for the "submit" event
   
+  event.preventDefault();
+  // Prevent the default form submission behavior
+  
+  document.getElementById("message").style.display = "block";
+  // Retrieve the element with ID "message" and change its CSS display property to "block"
+  // This will make the element visible 
+});
